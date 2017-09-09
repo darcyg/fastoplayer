f417048e3a376d06593d6bc9b6f6ca7987af0283
topilski <atopilski@gmail.com>
2017-09-09 18:05:07 +0300

    Init sources

---
+++ src/load_config.h
@@ -0,0 +1,32 @@
+/*  Copyright (C) 2014-2017 FastoGT. All right reserved.
+
+    This file is part of FastoTV.
+
+    FastoTV is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FastoTV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FastoTV. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <common/error.h>  // for Error
+
+#include "player/config.h"
+
+namespace fastotv {
+namespace client {
+
+common::ErrnoError load_config_file(const std::string& config_absolute_path,
+                                    player::TVConfig* options) WARN_UNUSED_RESULT;
+common::ErrnoError save_config_file(const std::string& config_absolute_path,
+                                    player::TVConfig* options) WARN_UNUSED_RESULT;
+
+}  // namespace client
+}  // namespace fastotv
